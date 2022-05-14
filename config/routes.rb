69964db6b223f14b9dc4b5172290e87a15a9fbb9
yamaguchi-yoshiki <102662423+yamaguchi-yoshiki@users.noreply.github.com>
2022-05-14 08:36:18 +0000

    [Add] add homes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  root to: 'public/homes#top'
+  get '/about' => 'public/homes#about', as: 'about'
+
+  scope module: :public do
+
+    resources :items, only: [:index, :show]
+  end
+
   namespace :admin do
     resources :users, only: [:index,:show,:edit,:update]
-    resources :items
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
   end
 
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
